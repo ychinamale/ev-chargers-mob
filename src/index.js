956c71caf010db6ecf3f885956e195df2c139379
Yamikani Chinamale <ychinamale@gmail.com>
2022-03-12 13:26:54 +0200

    Added Expo MapView

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the EV Charger Finder App</Text>
-      <StatusBar style="auto" />
+    <View style={styles.layout}>
+      <Text>Charger Finder</Text>
+      <View style={styles.mapContainer}>
+        <MapView 
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
+    alignItems: 'center',
+  },
+  mapContainer: {
+    height: 400,
+    width: 400,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
