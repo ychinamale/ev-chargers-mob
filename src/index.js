5ea039d7d83f38a804d22695d8224f05f2ba9c8c
Yamikani Chinamale <ychinamale@gmail.com>
2022-03-14 11:24:45 +0200

    Set default region and refactored location

--- src/index.js
+++ src/index.js
@@ -1,30 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = -26.130029273632143; // Somewhere in Joburg
+const LONGITUDE = 27.973646800965067; // Somewhere in Joburg
+const LATITUDE_DELTA = 0.0922; // from `examples/DisplayLatLng.js`
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO; // from `examples/DisplayLatLng.js`
+
 export default function App() {
-  const [location, setLocation] = React.useState({ data: null, isLoading: false });
+  const [region, setRegion] = React.useState({ data: null, isLoading: false });
 
   const handleCurrentUpdate = () => {
-    setLocation((prevLocation) => ({ data: { ...prevLocation?.data}, isLoading: true }))
+    setRegion((prevLocation) => ({ data: { ...prevLocation?.data }, isLoading: true }))
+  }
+
+  const handleRegionChange = (thisRegion) => {
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = thisRegion;
+
+    setRegion({
+      data: {
+        latitude,
+        longitude,
+        latitudeDelta,
+        longitudeDelta
+      },
+      isLoading: false
+    })
   }
 
   React.useEffect(() => {
-    if (!!location.isLoading) {
+    if (!!region.isLoading) {
       (async function getCurrentLocation () {
-        let { status } = await Location.requestForegroundPermissionsAsync();
+        const { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
           return console.log('Permission to access location was denied');
         }
 
-        let data = await Location.getCurrentPositionAsync({});
-        setLocation({ data, isLoading: false });
+        const data = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = data?.coords;
+
+        setRegion({ 
+          data: {
+            latitude,
+            longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }, 
+          isLoading: false });
       })()
     }
-  }, [location.isLoading])
+  }, [region.isLoading])
 
-  console.log('The location now is', JSON.stringify(location, null, 2));
+  console.log('The location now is', JSON.stringify(region, null, 2));
 
   return (
     <View style={styles.layout}>
@@ -33,9 +64,17 @@ export default function App() {
         <MapView 
           provider={PROVIDER_GOOGLE}
           style={styles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          region={region.data}
+          onRegionChangeComplete={(thisRegion) => { handleRegionChange(thisRegion) }}
         />
       </View>
-      <Text>Status: { !!location.isLoading ? '...loading': 'Done' }</Text>
+      <Text>Status: { !!region.isLoading ? '...loading': 'Done' }</Text>
       <TouchableOpacity style={styles.button} onPress={handleCurrentUpdate} >
         <Text>get current location</Text>
       </TouchableOpacity>
